ef97b1ae0957ef4aeb53334d3ffea99207e163b2
Pedro Brasileiro <pedrobrasileiro@gmail.com>
2010-11-20 18:40:40 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TesteTinymce::Application.config.secret_token = '995a3b5d783c310003a419fcd5e3370c0409919a304fe1c1177b846df31e41ee54a0b0630e16fe3352f3aee197e97de0b4b01b90123b7436f2cee2ad02376f62'
